e4224079349140b93caf4582195232868f943791
jzuluaga02 <jzuluaga@scu.edu>
2024-01-03 19:03:20 -0800

    evaluation with mlflow added to the model

---
+++ src/prueba.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/jzuluaga02/kidney-disease-predictor.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="jzuluaga02"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="b493059e326a65a4b4011c4db5c5a4cc40f37756"
