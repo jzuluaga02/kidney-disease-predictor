371d0d5b717bd73d8f3dbe1b6df167bbcebee011
jzuluaga02 <jzuluaga@scu.edu>
2024-02-27 21:44:04 -0800

    model working

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -35,11 +35,7 @@ class DataIngestion:
     
 
     def extract_zip_file(self):
-        """
-        zip_file_path: str
-        Extracts the zip file into the data directory
-        Function returns None
-        """
+      
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
